77143434e8022473d6af3762ff940a02bbad6cb0
Jason Hadwen <you@example.com>
2022-09-13 17:50:38 +0100

    changes

--- floyd_warshall_recursive.py
+++ floyd_warshall_recursive.py
@@ -5,7 +5,7 @@ import numpy as np
 #graph = np.random.rand((3, 3))
 
 NO_PATH = sys.maxsize
-graph = [[0, 'x', NO_PATH, 8],
+graph = [[0, 7, NO_PATH, 8],
         [NO_PATH, 0, 5, NO_PATH],
         [NO_PATH, NO_PATH, 0, 2],
         [NO_PATH, NO_PATH, NO_PATH, 0]]
