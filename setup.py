794aa449501e4e2d297eea38a1a30797f541ae0e
Jason Hadwen <you@example.com>
2022-09-16 16:08:25 +0100

    Arranging the project

--- setup.py
+++ setup.py
@@ -1,13 +1,14 @@
-from setuptools import setup, find_packages
+from distutils.core import setup
 
 setup(
-    name="floyd-recursive",
-    version="0.1",
-    author="Jason Hadwen",
-    author_email="sgjhadwe@liverpool.ac.uk",
-    url="https://github.com/jasonshhh/Mid-Module-Assessment",
-    py_modules=["mid_module_assessment/floyd_warshall_recursive", "mid_module_assessment/floyd_warshall_iterative"],
-    license="Apache 2.0",
-    python_version="3.10",
-    packages=find_packages(include=['mid_module_assessment'])
+    name='FloydWarshallRecursive',
+    version='0.1.0',
+    author = 'Jason Hadwen',
+    packages=['floydWarshall', 'tests',],
+    scripts = ['floyd_warshall_imperative.py', 'floyd_warshall_recursive.py'],
+    url = 'https://github.com/jasonshhh/Mid-Module-Assessment',
+    license='Apache 2.0',
+    description = 'Rewriting the Floyd Warshall Algorithm to use recursion',
+    long_description=open('README.md').read(),
+    install_requires = "python >= 3.1.0",
 )
