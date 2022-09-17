491b8ad259eef5b0dc857692f46e7a6969cb04c3
Jason Hadwen <you@example.com>
2022-09-17 22:32:12 +0100

    m

--- tests/unittest_floyd.py
+++ tests/unittest_floyd.py
@@ -11,12 +11,12 @@ from floydWarshall.floyd_warshall_imperative import floyd
 
 # from floyd_warshall_recursive import printSolution
 
-no_path = sys.maxsize
+NO_PATH = sys.maxsize
 graph = [
-        [0, 7, no_path, 8],
-        [no_path, 0, 5, no_path],
-        [no_path, no_path, 0, 2],
-        [no_path, no_path, no_path, 0]
+        [0, 7, NO_PATH, 8],
+        [NO_PATH, 0, 5, NO_PATH],
+        [NO_PATH, NO_PATH, 0, 2],
+        [NO_PATH, NO_PATH, NO_PATH, 0]
         ]
 
 VERTICES = len(graph)
